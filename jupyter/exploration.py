99b8b775b23f2ac9f3877b48827e821d929cdd0e
athanikos <athanikos@gmail.com>
2019-08-24 19:39:05 +0300

    Update exploration.py

--- jupyter/exploration.py
+++ jupyter/exploration.py
@@ -13,32 +13,44 @@ df_lastGoal60orMore = df
 pd.set_option('display.max_rows', 1500)
 df_Eredevisie =  df_lastGoal60orMore[df_lastGoal60orMore.league_name == 'Eredivisie'  ]
 df_Eredevisie.groupby(['differenceAt75'])['goalScoredLast15'].sum() / df_Eredevisie.groupby(['differenceAt75']).size()
-#53% at -1 
+#54% at -1 , 52% @ 1 
 
-# all leagues at -1 , pro league & eredivisie & copa italia 
+
+
+# differenceAt75 , awayGoalsUpTo75 
+# more than 53% when -1,1 and awaygoals = 1,2 
 df_lastGoal60orMore = df
 pd.set_option('display.max_rows', 1500)
-df_Eredevisie =  df_lastGoal60orMore
-df_Eredevisie.groupby(['differenceAt75','positionDifferenceWithin3','homeTeamIBetter'])['goalScoredLast15'].sum() / df_Eredevisie.groupby(['differenceAt75','positionDifferenceWithin3','homeTeamIBetter']).size()
+df_Eredevisie =  df_lastGoal60orMore[df_lastGoal60orMore.league_name == 'Eredivisie'  ]
+df_Eredevisie.groupby(['differenceAt75','awayGoalsUpTo75'])['goalScoredLast15'].sum() / df_Eredevisie.groupby(['differenceAt75','awayGoalsUpTo75']).size()
 
-
-# check size 
+# differenceAt75 , homeGoalsUpTo75 
+# -2 : 57% when 1,2 
+#-1 53,55,53
 df_lastGoal60orMore = df
 pd.set_option('display.max_rows', 1500)
-df_Eredevisie =  df_lastGoal60orMore
-df_Eredevisie.groupby(['league_name','differenceAt75']).size()
+df_Eredevisie =  df_lastGoal60orMore[df_lastGoal60orMore.league_name == 'Eredivisie'  ]
+df_Eredevisie.groupby(['differenceAt75','homeGoalsUpTo75'])['goalScoredLast15'].sum() / df_Eredevisie.groupby(['differenceAt75','homeGoalsUpTo75']).size()
+# both  illustrate that when diff = 0 chances are @45%
 
-# per team this gives some numbers 
+
+
+# per team 
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from pymongo import MongoClient
 client = MongoClient("localhost", 27017, maxPoolSize=50)
 db = client.book
-collection = db['events']
+collection = db['FullTimeMatches']
 cursor = collection.find()
 df =  pd.DataFrame(list(cursor))
 df_lastGoal60orMore = df
 pd.set_option('display.max_rows', 1500)
 df_Eredevisie =  df_lastGoal60orMore[df_lastGoal60orMore.league_name == 'Eredivisie'  ]
 df_Eredevisie.groupby(['differenceAt75','hometeamname'])['goalScoredLast15'].sum() / df_Eredevisie.groupby(['differenceAt75','hometeamname']).size()
+
+# check sizes 
+df_Eredevisie.groupby(['differenceAt75','hometeamname'])['goalScoredLast15'].size()
+
+
