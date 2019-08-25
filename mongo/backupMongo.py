e7282184aae0612d6a2ebb2ce2be4b81b06b07f5
athanikos <athanikos@gmail.com>
2019-08-25 14:50:41 +0300

    Create backupMongo.py

---
+++ mongo/backupMongo.py
@@ -0,0 +1,35 @@
+
+
+#https://stackoverflow.com/questions/23894221/upload-file-to-my-dropbox-from-python-script/36851978#36851978
+#!/usr/bin/env python
+import dropbox
+
+class TransferData:
+    def __init__(self, access_token):
+        self.access_token = access_token
+
+    def upload_file(self, file_from, file_to):
+        """upload a file to Dropbox using API v2
+        """
+        dbx = dropbox.Dropbox(self.access_token)
+
+        with open(file_from, 'rb') as f:
+            dbx.files_upload(f.read(), file_to)
+
+def main():
+    access_token = '************'
+    transferData = TransferData(access_token)
+
+    file_from = 'test.txt'
+    file_to = '/test_dropbox/test.txt'  # The full path to upload the file to, including the file name
+
+    # API v2
+    transferData.upload_file(file_from, file_to)
+
+dbx = dropbox.Dropbox('******')
+print(dbx.users_get_current_account().name)
+
+
+
+#if __name__ == '__main__':
+#    main()
