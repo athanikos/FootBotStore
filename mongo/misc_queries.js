98aa62defd44019545606fe60e13cf74e2ef1501
athanikos <athanikos@gmail.com>
2019-08-26 17:27:59 +0300

    Update misc_queries.js

--- mongo/misc_queries.js
+++ mongo/misc_queries.js
@@ -1,2 +1,14 @@
- 
+ //removes average stats 
  db.FullTimeMatches.update(   {}, { $unset: { AverageAwayGoalsUpTo75:"", AverageHomeGoalsUpTo75: "" } } , {multi: true})
+
+//get dsitinct teams 
+db.FullTimeMatches.aggregate(
+    [
+         {$match : { "league_name":"Eredivisie"}},  
+          {
+            $group: {
+              _id: "$hometeamname"
+            }
+          }
+   ]
+)
