15578eb06eecd0ccf910bb8640ac16cf22dc2db5
athanikos <athanikos@gmail.com>
2019-08-31 02:36:05 +0300

    Update Decisiontree.py

--- jupyter/Decisiontree.py
+++ jupyter/Decisiontree.py
@@ -2,6 +2,13 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from pymongo import MongoClient
+from io import StringIO 
+from IPython.display import Image 
+import pydotplus
+from sklearn import preprocessing
+from sklearn import tree
+%matplotlib inline
+
 client = MongoClient("localhost", 27017, maxPoolSize=50)
 db = client.book
 collection = db['FullTimeMatches']
@@ -11,36 +18,44 @@ pd.set_option('display.max_rows', 5000)
 pd.options.display.max_columns = None
 df_Eredevisie =  df[df.league_name == 'Eredivisie'  ]
 
-df_minimal_Eredevisie = df_Eredevisie[[  'visitorTeamId'
-                                        ,'localTeamId'
-                                         ,'HomeTeamAverageAttack'
-                                        ,'HomeTeamAverageDefence'
-                                        ,'AwayTeamAverageAttack'
-                                        ,'AwayTeamAverageDefence'
-                                        ,'differenceAt75'
-                         
-                                        ,'goalScoredLast15'   
-                        
-                                      ]].copy()
-df_minimal_Eredevisie.visitorTeamId = pd.to_numeric(df_minimal_Eredevisie.visitorTeamId, errors='coerce').fillna(0).astype(np.int64)
 
-df_minimal_Eredevisie.localTeamId = pd.to_numeric(df_minimal_Eredevisie.localTeamId, errors='coerce').fillna(0).astype(np.int64)
+df_minimal_Eredevisie = df_Eredevisie[[ 'startingatwithtime', 'hometeamname','awayteamname'
+                                        ,'HomeTeamWhileHomeAverageAttackUpTo75'
+                                        ,'HomeTeamWhileHomeAverageDefenceUpTo75'
+                                        ,'HomeTeamWhileHomeAverageAttackLast15'
+                                        ,'HomeTeamWhileHomeAverageDefenceLast15'
+                                        ,'AwayTeamWhileAwayAverageAttackUpTo75'
+                                        ,'AwayTeamWhileAwayAverageDefenceUpTo75'
+                                        ,'AwayTeamWhileAwayAverageAttackLast15'
+                                        ,'AwayTeamWhileAwayAverageDefenceLast15'
+                                        ,'differenceAt75'
+                                        ,'homeGoalsUpTo75'
+                                        ,'awayGoalsUpTo75'
+                                        ,'goalScoredLast15'   
+                                      ]].copy()
+
 
 import pandas as pd
 from sklearn.tree import DecisionTreeClassifier # Import Decision Tree Classifier
 from sklearn.model_selection import train_test_split # Import train_test_split function
 from sklearn import metrics #Import scikit-learn metrics module for accuracy calculation
 
+
+
+def plot_decision_tree(clf,feature_name,target_name):
+    dot_data = StringIO()  
+    tree.export_graphviz(clf, out_file=dot_data,  
+                         feature_names=feature_name,  
+                         class_names=target_name,  
+                         filled=True, rounded=True,  
+                         special_characters=True)  
+    graph = pydotplus.graph_from_dot_data(dot_data.getvalue())  
+    return Image(graph.create_png())
 #https://www.datacamp.com/community/tutorials/decision-tree-classification-python
 feature_cols = [                   
-                                        'visitorTeamId'
-                                        ,'localTeamId'
-                                        ,'HomeTeamAverageAttack'
-                                        ,'HomeTeamAverageDefence'
-                                        ,'AwayTeamAverageAttack'
-                                        ,'AwayTeamAverageDefence'
-                                        ,'differenceAt75'
-                                        ]
+                                         'differenceAt75'
+                                        ,'homeGoalsUpTo75'
+                                        ,'awayGoalsUpTo75' ]
                                         
 X = df_minimal_Eredevisie[feature_cols] # Features
 y = df_minimal_Eredevisie.goalScoredLast15 # Target variable
@@ -58,3 +73,5 @@ y_pred = clf.predict(X_test)
 
 
 print("Accuracy:",metrics.accuracy_score(y_test, y_pred))
+
+plot_decision_tree(clf, X_train.columns,df_minimal_Eredevisie.columns[1])
