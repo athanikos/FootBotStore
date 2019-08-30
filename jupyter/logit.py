97201851b36696ce03e7744728b5002bfa168670
athanikos <athanikos@gmail.com>
2019-08-31 01:30:37 +0300

    Update logit.py

--- jupyter/logit.py
+++ jupyter/logit.py
@@ -1,6 +1,3 @@
-import pandas as pd 
-import numpy as np
-import matplotlib.pyplot as plt
 from pymongo import MongoClient
 client = MongoClient("localhost", 27017, maxPoolSize=50)
 db = client.book
@@ -10,7 +7,6 @@ df =  pd.DataFrame(list(cursor))
 pd.set_option('display.max_rows', 5000)
 pd.options.display.max_columns = None
 df_Eredevisie =  df[df.league_name == 'Eredivisie'  ]
-
 
 df_minimal_Eredevisie = df_Eredevisie[[
                                         'HomeTeamWhileHomeAverageAttackUpTo75'
@@ -26,10 +22,6 @@ df_minimal_Eredevisie = df_Eredevisie[[
                                         ,'awayGoalsUpTo75'
                                         ,'goalScoredLast15'   
                                       ]].copy()
-
-df_minimal_Eredevisie.dtypes
-
-
 
 feature_cols = [                 
                                         'HomeTeamWhileHomeAverageAttackUpTo75'
@@ -49,8 +41,20 @@ y =df_minimal_Eredevisie['goalScoredLast15']
 X=df_minimal_Eredevisie[feature_cols]
 
 
+from sklearn.model_selection  import train_test_split 
 
-import statsmodels.api as sm
-logit_model=sm.Logit(y,X)
-result=logit_model.fit()
-print(result.summary2())
+X_train, X_test, y_train , y_test = train_test_split(X,y,test_size=0.3, random_state=1)
+from sklearn.linear_model import LogisticRegression 
+logmodel = LogisticRegression() 
+logmodel.fit(X_train,y_train)
+
+predictions = logmodel.predict(X_test)
+from sklearn.metrics import classification_report 
+classification_report(y_test,predictions)
+
+from sklearn.metrics import confusion_matrix 
+confusion_matrix(y_test,predictions)
+
+
+
+
