756b916ab4c3af4b13c1338324eb38f622079362
athanikos <athanikos@gmail.com>
2019-08-31 02:25:55 +0300

    Update Decisiontree.py

--- jupyter/Decisiontree.py
+++ jupyter/Decisiontree.py
@@ -11,22 +11,20 @@ pd.set_option('display.max_rows', 5000)
 pd.options.display.max_columns = None
 df_Eredevisie =  df[df.league_name == 'Eredivisie'  ]
 
-
-df_minimal_Eredevisie = df_Eredevisie[[ 'startingatwithtime', 'hometeamname','awayteamname'
-                                        ,'HomeTeamWhileHomeAverageAttackUpTo75'
-                                        ,'HomeTeamWhileHomeAverageDefenceUpTo75'
-                                        ,'HomeTeamWhileHomeAverageAttackLast15'
-                                        ,'HomeTeamWhileHomeAverageDefenceLast15'
-                                        ,'AwayTeamWhileAwayAverageAttackUpTo75'
-                                        ,'AwayTeamWhileAwayAverageDefenceUpTo75'
-                                        ,'AwayTeamWhileAwayAverageAttackLast15'
-                                        ,'AwayTeamWhileAwayAverageDefenceLast15'
+df_minimal_Eredevisie = df_Eredevisie[[  'visitorTeamId'
+                                        ,'localTeamId'
+                                         ,'HomeTeamAverageAttack'
+                                        ,'HomeTeamAverageDefence'
+                                        ,'AwayTeamAverageAttack'
+                                        ,'AwayTeamAverageDefence'
                                         ,'differenceAt75'
-                                        ,'homeGoalsUpTo75'
-                                        ,'awayGoalsUpTo75'
+                         
                                         ,'goalScoredLast15'   
+                        
                                       ]].copy()
+df_minimal_Eredevisie.visitorTeamId = pd.to_numeric(df_minimal_Eredevisie.visitorTeamId, errors='coerce').fillna(0).astype(np.int64)
 
+df_minimal_Eredevisie.localTeamId = pd.to_numeric(df_minimal_Eredevisie.localTeamId, errors='coerce').fillna(0).astype(np.int64)
 
 import pandas as pd
 from sklearn.tree import DecisionTreeClassifier # Import Decision Tree Classifier
@@ -35,9 +33,14 @@ from sklearn import metrics #Import scikit-learn metrics module for accuracy cal
 
 #https://www.datacamp.com/community/tutorials/decision-tree-classification-python
 feature_cols = [                   
-                                         'differenceAt75'
-                                        ,'homeGoalsUpTo75'
-                                        ,'awayGoalsUpTo75' ]
+                                        'visitorTeamId'
+                                        ,'localTeamId'
+                                        ,'HomeTeamAverageAttack'
+                                        ,'HomeTeamAverageDefence'
+                                        ,'AwayTeamAverageAttack'
+                                        ,'AwayTeamAverageDefence'
+                                        ,'differenceAt75'
+                                        ]
                                         
 X = df_minimal_Eredevisie[feature_cols] # Features
 y = df_minimal_Eredevisie.goalScoredLast15 # Target variable
