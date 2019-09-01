d541e0a06e54e7bee9ea024ef8886dad2ea6b50c
athanikos <athanikos@gmail.com>
2019-09-01 03:08:55 +0300

    Update logit.py

--- jupyter/logit.py
+++ jupyter/logit.py
@@ -1,6 +1,3 @@
-import pandas as pd 
-import numpy as np
-import matplotlib.pyplot as plt
 from pymongo import MongoClient
 client = MongoClient("localhost", 27017, maxPoolSize=50)
 db = client.book
@@ -11,8 +8,8 @@ pd.set_option('display.max_rows', 5000)
 pd.options.display.max_columns = None
 df_Eredevisie =  df[df.league_name == 'Eredivisie'  ]
 
-df_minimal_Eredevisie = df_Eredevisie[[
-                                        'HomeTeamWhileHomeAverageAttackUpTo75'
+df_minimal_Eredevisie = df_Eredevisie[[ 'startingatwithtime', 'hometeamname','awayteamname'
+                                        ,'HomeTeamWhileHomeAverageAttackUpTo75'
                                         ,'HomeTeamWhileHomeAverageDefenceUpTo75'
                                         ,'HomeTeamWhileHomeAverageAttackLast15'
                                         ,'HomeTeamWhileHomeAverageDefenceLast15'
@@ -20,24 +17,40 @@ df_minimal_Eredevisie = df_Eredevisie[[
                                         ,'AwayTeamWhileAwayAverageDefenceUpTo75'
                                         ,'AwayTeamWhileAwayAverageAttackLast15'
                                         ,'AwayTeamWhileAwayAverageDefenceLast15'
+                                        ,'HomeTeamWhileHomeAverageAttack'
+                                        ,'HomeTeamWhileHomeAverageDefence'
+                                        ,'AwayTeamWhileAwayAverageAttack'
+                                        ,'AwayTeamWhileAwayAverageDefence'
                                         ,'differenceAt75'
-                                        ,'homeGoalsUpTo75'
-                                        ,'awayGoalsUpTo75'
+                                        ,'redDifferenceAt75'
+                                        ,'CombinedWhileHomeAverage'
+                                        ,'CombinedWhileAwayAverage'
+                                        ,'CombinedWhileAwayAverageUpTo75'
+                                        ,'CombinedWhileAwayAverageLast15'
+                                        ,'CombinedWhileHomeAverageUpTo75'
+                                        ,'CombinedWhileHomeAverageLast15'
                                         ,'goalScoredLast15'   
                                       ]].copy()
 
-feature_cols = [                 
-                                        'HomeTeamWhileHomeAverageAttackUpTo75'
-                                        ,'HomeTeamWhileHomeAverageDefenceUpTo75'
-                                        ,'HomeTeamWhileHomeAverageAttackLast15'
-                                        ,'HomeTeamWhileHomeAverageDefenceLast15'
-                                        ,'AwayTeamWhileAwayAverageAttackUpTo75'
-                                        ,'AwayTeamWhileAwayAverageDefenceUpTo75'
-                                        ,'AwayTeamWhileAwayAverageAttackLast15'
-                                        ,'AwayTeamWhileAwayAverageDefenceLast15'
-                                        ,'differenceAt75'
-                                        ,'homeGoalsUpTo75'
-                                        ,'awayGoalsUpTo75'
+
+df_minimal_Eredevisie['HomeAttack'] =    df_minimal_Eredevisie['HomeTeamWhileHomeAverageAttack'] + df_minimal_Eredevisie['HomeTeamWhileHomeAverageDefence']
+
+df_minimal_Eredevisie['AwayAttack'] =    df_minimal_Eredevisie['AwayTeamWhileAwayAverageAttack'] + df_minimal_Eredevisie['HomeTeamWhileHomeAverageDefence']
+
+df_minimal_Eredevisie['HomeAttackLast15'] =  df_minimal_Eredevisie['HomeTeamWhileHomeAverageAttackLast15'] 
++ df_minimal_Eredevisie['AwayTeamWhileAwayAverageDefenceLast15']
+
+
+df_minimal_Eredevisie['AwayAttackLast15'] =  df_minimal_Eredevisie['AwayTeamWhileAwayAverageAttackLast15'] 
++ df_minimal_Eredevisie['HomeTeamWhileHomeAverageDefenceLast15']
+
+
+
+feature_cols = [                         'HomeAttack'
+                                          ,'AwayAttack'
+                                   
+                                          ,'differenceAt75'
+                                          ,'redDifferenceAt75'
                                          ]
 
 y =df_minimal_Eredevisie['goalScoredLast15']
@@ -57,7 +70,3 @@ classification_report(y_test,predictions)
 
 from sklearn.metrics import confusion_matrix 
 confusion_matrix(y_test,predictions)
-
-
-
-
