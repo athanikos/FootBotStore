923529fcd469fc4d01c0cdf43e5c350a5d881f7e
athanikos <athanikos@gmail.com>
2019-09-10 23:50:12 +0300

    Update misc_queries.js

--- mongo/misc_queries.js
+++ mongo/misc_queries.js
@@ -16,3 +16,17 @@ db.FullTimeMatches.find( {$and: [{"hometeamname":"NEC", "league_name":"Eredivisi
    .projection({})
    .sort({starting_at:-1})
    .limit(100)
+
+
+// count matches with stats on 74 minute
+db.Matches.aggregate([
+{ $match: {"time.minute": 74 }},
+{ $match: {"stats.data": { $elemMatch: { $exists: true } }} },
+
+                    {
+                                    $project: {_id: 1,
+                                    count: {$size: '$stats.data'},
+                                    data: '$stats.data' }
+                    },
+                    { $match: {count: 2 }},
+                     { $group: { _id: null, count: { $sum: 1 } } }
