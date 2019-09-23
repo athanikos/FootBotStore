40028420e91ea4769a80bf0a2fe149f67379bacb
athanikos <athanikos@gmail.com>
2019-09-23 22:59:35 +0300

    Delete indexes.js

--- mongo/indexes.js
+++
@@ -1,5 +0,0 @@
-    
-    db.getCollection("Matches").createIndex({ "id": 1, "time.status": 1, "time.minute": 1 })
-    db.getCollection("Stat").createIndex({ "id": 1, "time.status": 1, "time.minute": 1 })
-
-    
