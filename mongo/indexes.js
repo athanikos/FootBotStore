40bb3bf96a308e0536d97ac4e651352be03c07f8
athanikos <athanikos@gmail.com>
2019-09-23 22:58:19 +0300

    Update indexes.js

--- mongo/indexes.js
+++ mongo/indexes.js
@@ -1,3 +1,5 @@
     
     db.getCollection("Matches").createIndex({ "id": 1, "time.status": 1, "time.minute": 1 })
+    db.getCollection("Stat").createIndex({ "id": 1, "time.status": 1, "time.minute": 1 })
+
     
