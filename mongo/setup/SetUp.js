a38abbcd8a18a7107903b5dd95c9caf0eb9ec441
athanikos <athanikos@gmail.com>
2020-03-04 21:43:46 +0200

    Update SetUp.js

--- mongo/setup/SetUp.js
+++ mongo/setup/SetUp.js
@@ -1,3 +1,13 @@
+db.createUser(
+    {
+        user: "foot_clone",
+        pwd: "<enterPassword>",
+        customData: {},
+        roles: [{ "role": "userAdminAnyDatabase", "db": "admin" }, { "role": "readWrite", "db": "book" }],
+    }
+)
+
+
 db.grantRolesToUser(
     "foot",
     [
