0332ac0f1f4be1da7af464c9952383f4b38b6cdd
athanikos <athanikos@gmail.com>
2020-03-15 20:04:07 +0200

    Update checkForDuplicateGames.js

--- mongo/stats/checkForDuplicateGames.js
+++ mongo/stats/checkForDuplicateGames.js
@@ -6,12 +6,22 @@ db.matches.aggregate( [
     {$match: {'time.status' : 'FT' }},
    { $group: { _id: { id: "$id", status: "$time.status" },
                count: { $sum: 1 } } },
-   { $match: { count: { $gt: 1 } } },
-   { $project: { _id: 0, 
-                 id: "$_id.id", 
-                 status: "$_id.status", 
-                 count: 1}}
-] )
+   { $match: { count: { $eq: 1 } } },
+
+] ).count()
 
 
 db.matches.find({$and:[{'id':11914352}, {'time.status':'FT'}] })
+
+
+db.flatmatches.count() //331922
+db.flatmatches.find({'time_status':'FT'}).count()
+
+db.flatmatches.aggregate( [
+    {$match: {'time_status' : 'FT' }},
+   { $group: { _id: { id: "$id", status: "$time_status" },
+               count: { $sum: 1 } } },
+   { $match: { count: { $eq: 1 } } },
+
+] ).count()
+
