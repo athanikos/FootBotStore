87b954f92e0a9ce95c809b24cb1eb99f856ae124
athanikos <athanikos@gmail.com>
2020-03-15 20:26:58 +0200

    Update indexes.js

--- mongo/setup/indexes.js
+++ mongo/setup/indexes.js
@@ -3,5 +3,5 @@
     db.getCollection("Stats").createIndex({ "id": 1, "time.status": 1, "time.minute": 1 })
 
     db.getCollection("flatmatches").createIndex({ "league_id": 1, "localteam_id": 1 })
-db.getCollection("flatmatches").createIndex({ "league_id": 1, "visitorteam_id": 1 })
+    db.getCollection("flatmatches").createIndex({ "league_id": 1, "visitorteam_id": 1 })
 
