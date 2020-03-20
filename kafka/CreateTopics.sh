05deb744eae81ec1893f4d7975bcd08ac8f65d05
athanikos <athanikos@gmail.com>
2020-03-20 12:45:03 +0200

    Update and rename create_topics.sh to CreateTopics.sh

---
+++ kafka/CreateTopics.sh
@@ -0,0 +1,3 @@
+home/kafka/kafka/bin/kafka-topics.sh --create  --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic FootBotApiLogs
+
+--todo enter the rest 
--- kafka/create_topics.sh
+++
@@ -1,1 +0,0 @@
-bin/kafka-topics.sh --create --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 --topic FootBotApiLogs
